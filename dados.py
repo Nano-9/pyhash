cd7b5fa39a5a1a551553c0f74db9be7f6a42b42c
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2021-04-19 19:09:25 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,18 @@
+dados = ['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','s','ç','aa','ai','ao','au','ba','be','bi','bo','bu','ca','ce','ci','co','cu','da','de','di','do','du',
+		'ea','ee','ei','eo','eu','fa','fe','fi','fo','fu','ga','ge','gi','go','gu','ha','he','hi','ho','hu','ia','ie','ii','io','iu','ja','je','ji','jo','ju',
+		'ka','ke','ki','ko','ku','la','le','li','lo','lu','ma','me','mi','mo','mu','na','ne','ni','no','nu','oa','oe','oi','oo','ou','pa','pa','pe','pi','po','pu',
+		'qua','que','qui','quo','quu','ra','re','ri','ro','ru','sa','se','si','so','su','ta','te','ti','to','tu','ua','ue','ui','uo','uu','va','ve','vi','vo','vu',
+		'wa','we','wi','wo','wu','xa','xe','xi','xo','xu','ya','ye','yi','yo','yu','za','ze','zi','zo','zu','ç','cha','che','chi','cho','chu','ban','ben','bin','bon','bun','can','cen','cin','con',
+		'cun','dan','den','din','don','dun','ean','een','ein','eon','eun','fan','fen','fin','fon','fun','gan','gen','gin','gon','gun','han','hen','hin','hon',
+		'hun','ida','idi','ide','ido','idu','ira','ire','iri','iro','iru','jan','jen','jin','jon','jun','kan','ken','kin','kon','kun','lan','len','lin','lon','lun','man','men','min','mon','mun',
+		'nan','nen','nin','non','nun','oan','oen','oin','oon','oun','pan','pan','pen','pin','pon','pun','qua','que','qui','quo','quu','ran','ren','rin','ron',
+		'run','san','sen','sin','son','sun','tan','ten','tin','ton','tun','uan','uen','uin','uon','uun','van','ven','vin','von','vun','wan','wen','win','won','wun','xan',
+		'xen','xin','xon','xun','yan','yen','yin','yon','yun','yes','zan','zen','zin','zon','zun','bam','bem','bim','bom','bum','cam','cem','cim','com',
+		'cum','dam','dem','dim','dom','dum','eam','eem','eim','eom','eum','fam','fem','fim','fom','fum','gam','gem','gim','gom','gum','ham','hem','him','hom',
+		'hum','iam','iem','iim','iom','ium','jam','jem','jim','jom','jum','kam','kem','kim','kom','kum','lam','lem','lim','lom','lum','mam','mem','mim','mom','mum',
+		'nam','nem','nim','nom','num','oam','oem','oim','oom','oum','pam','pam','pem','pim','pom','pum','ram','rem','rim','rom','lha','lhe','lhi','lho','lhu','nha','nhe','nhi','nho','nhu',
+		'rum','sam','sem','sim','som','sum','tam','tem','tim','tom','tum','uam','uem','uim','uom','uum','vam','vem','vim','vom','vum','wam','wem','wim','wom','wum','xam',
+		'xem','xim','xom','xum','yam','yem','yim','yom','yum','yes','zam','zem','zim','zom','zum','rra','rre','rri','rro','rru','ssa','sse','ssi','sso','ssu',
+		'1','2','3','4','5','6','7','8','9','0','01','02','03','04','05','06','07','08','09','00','10','20','30','40','50','60','70','80','90','100','_','@','#','$','!','%','&','*']
+
+dadoos = list("abcdefghijklmnopqrstuvwxyz0123456789")
