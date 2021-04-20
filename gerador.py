9a57379be430f95ce641cfd55dcf6b7acb1aae65
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2021-04-19 23:18:27 -0300

    Update gerador.py

--- gerador.py
+++ gerador.py
@@ -26,33 +26,30 @@ if len(logar) == 2:
 		time.sleep(1.2)
 		os.system("clear")
 		print("""\033[94m
-                           _                  
-   __ _  ___ _ __ __ _  __| | ___  _ __       
-  / _` |/ _ \ '__/ _` |/ _` |/ _ \| '__|      
- | (_| |  __/ | | (_| | (_| | (_) | |         
-  \__, |\___|_|  \__,_|\__,_|\___/|_|       _ 
-  |___/  __ _ ___ _____      _____  _ __ __| |
- | '_ \ / _` / __/ __\ \ /\ / / _ \| '__/ _` |
- | |_) | (_| \__ \__ \\ V  V / (_) | | | (_| |
- | .__/ \__,_|___/___/ \_/\_/ \___/|_|  \__,_|
- |_|\033[m
-                versão 1.0
-
-coded by: lucas-DK
-Meu github: https://github.com/lucas-Dk
+ _______  __   __  __   __  _______  _______  __   __ 
+|       ||  | |  ||  | |  ||   _   ||       ||  | |  |
+|    _  ||  |_|  ||  |_|  ||  |_|  ||  _____||  |_|  |
+|   |_| ||       ||       ||       || |_____ |       |
+|    ___||_     _||       ||       ||_____  ||       |
+|   |      |   |  |   _   ||   _   | _____| ||   _   |
+|___|      |___|  |__| |__||__| |__||_______||__| |__|\033[m
+                                                                    
+> versão 1.0
+> coded by: lucas-DK
+> meu github: https://github.com/lucas-Dk
 
 
-MENU
+==================== MENU =======================\n
+\033[91m[\033[m\033[34mA\033[m\033[91m]\033[m - Gerar senhas com 1 sílaba  \033[92m[RÁPIDO]\033[m
+\033[91m[\033[m\033[34mB\033[m\033[91m]\033[m - Gerar senhas com 2 sílabas \033[92m[RÁPIDO]\033[m
+\033[91m[\033[m\033[34mC\033[m\033[91m]\033[m - Gerar senhas com 4 sílabas \033[93m[LENTO]\033[m
+\033[91m[\033[m\033[34mD\033[m\033[91m]\033[m - Gerar senhas com 6 sílabas \033[91m[MUITO LENTO]\033[m
+\033[91m[\033[m\033[34mE\033[m\033[91m]\033[m - Gerar senhas com 8 sílabas \033[91m[MUITO LENTO]\033[m
+\033[91m[\033[m\033[34mF\033[m\033[91m]\033[m - Gerar senhas aleatorias    \033[91m[MUITO LENTO]\033[m
+\033[91m[\033[m\033[34mX\033[m\033[91m]\033[m - Fechar programa
 
-\033[91m[\033[m\033[32mA\033[m\033[91m]\033[m - Gerar senhas com 1 sílaba  \033[92m[RÁPIDO]\033[m
-\033[91m[\033[m\033[32mB\033[m\033[91m]\033[m - Gerar senhas com 2 sílabas \033[92m[RÁPIDO]\033[m
-\033[91m[\033[m\033[32mC\033[m\033[91m]\033[m - Gerar senhas com 4 sílabas \033[93m[LENTO]\033[m
-\033[91m[\033[m\033[32mD\033[m\033[91m]\033[m - Gerar senhas com 6 sílabas \033[91m[MUITO LENTO]\033[m
-\033[91m[\033[m\033[32mE\033[m\033[91m]\033[m - Gerar senhas com 8 sílabas \033[91m[MUITO LENTO]\033[m
-\033[91m[\033[m\033[32mF\033[m\033[91m]\033[m - Gerar senhas aleatorias    \033[91m[MUITO LENTO]\033[m
-\033[91m[\033[m\033[32mX\033[m\033[91m]\033[m - Fechar programa
-
-			""")
+=================================================\n
+""")
 		try:
 			userop = str(input("O que você deseja: ")).upper()[0]
 		except KeyboardInterrupt:
@@ -72,26 +69,29 @@ MENU
 
 			if userop == "A":
 				quantidade_senhas_geradas = 0
-				print("\033[31mGerando as suas senhas...\033[m\n")
+				print("\033[1;31m[+] Gerando as suas senhas... [+]\033[m\n")
+				print("\033[1;33m[+] Tempo estimado: 6.0 segundos\033[m")
 				for z in palavra_para_senhas:
 					quantidade_senhas_geradas += 1
 					armazenar = z
 					funcoes.adicionar(nomearq=arquivo,password=armazenar)
-				print("\033[32mStatus do arquivo \033[35m{}\033[m: \033[32mCRIADO\033[m".format(arquivo))
+				print("\033[32m[+] Status do arquivo\033[35m{}\033[m:\033[1;36mConcluído\033[m".format(arquivo))
 				print("\033[32mQuantidade de senhas no arquivo:\033[m {}".format(quantidade_senhas_geradas))
 
 			elif userop == "B":
-				print("\033[31mGerando as suas senhas...\033[m")
+				print("\033[1;31m[+] Gerando as suas senhas... [+]\033[m\n")
+				print("\033[1;33m[+] Tempo estimado: 1.5 minutos\033[m")
 				for x in palavra_para_senhas:
 					for z in palavra_para_senhas:
 						quantidade_senhas_geradas += 1
 						armazenar = x+z
 						funcoes.adicionar(nomearq=arquivo,password=armazenar)
-				print("\033[32mStatus do arquivo \033[35m{}\033[m: \033[32mCRIADO\033[m".format(arquivo))
+				print("\033[32m[+] Status do arquivo\033[35m{}\033[m:\033[1;36mConcluído\033[m".format(arquivo))
 				print("\033[32mQuantidade de senhas no arquivo:\033[m {}".format(quantidade_senhas_geradas))
 
 			elif userop == "C":
-				print("\033[31mGerando as suas senhas, este processo pode demorar um pouco...\033[m\n")
+				print("\033[1;31m[+] Gerando as suas senhas, este processo pode demorar um pouco...\033[m\n")
+				print("\033[1;33m[+] Tempo estimado: 7 minutos\033[m")
 				for x in palavra_para_senhas:
 					for z in palavra_para_senhas:
 						for a in palavra_para_senhas:
@@ -99,11 +99,12 @@ MENU
 								quantidade_senhas_geradas += 1
 								armazenar = x+z+a+b
 								funcoes.adicionar(nomearq=arquivo,password=armazenar)
-				print("\033[32mStatus do arquivo \033[35m{}\033[m: \033[32mCRIADO\033[m".format(arquivo))
+				print("\033[32m[+] Status do arquivo\033[35m{}\033[m:\033[1;36mConcluído\033[m".format(arquivo))
 				print("\033[32mQuantidade de senhas no arquivo:\033[m {}".format(quantidade_senhas_geradas))
 				
 			elif userop == "D":
-				print("\033[31mGerando as suas senhas, este processo pode demorar...\033[m\n")	
+				print("\033[1;31m[+] Gerando as suas senhas, este processo pode demorar...\033[m\n")
+				print("\033[1;33m[+] Tempo estimado: 5 horas {depende do dispositivo}\033[m")	
 				for x in palavra_para_senhas:
 					for z in palavra_para_senhas:
 						for a in palavra_para_senhas:
@@ -113,11 +114,12 @@ MENU
 										quantidade_senhas_geradas += 1
 										armazenar = x+z+a+b+c+d
 										funcoes.adicionar(nomearq=arquivo,password=armazenar)
-				print("\033[32mStatus do arquivo \033[35m{}\033[m: \033[32mCRIADO\033[m".format(arquivo))
+				print("\033[32m[+] Status do arquivo\033[35m{}\033[m:\033[1;36mConcluído\033[m".format(arquivo))
 				print("\033[32mQuantidade de senhas no arquivo:\033[m {}".format(quantidade_senhas_geradas))
 				
 			elif userop == "E":
-				print("\033[31mGerando as suas senhas, este processo pode demorar...\033[m\n")
+				print("\033[1;31m[+] Gerando as suas senhas, este processo pode demorar...\033[m\n")
+				print("\033[1;33m[+] Tempo estimado: 6 horas {depende do dispositivo}\033[m")
 				for x in palavra_para_senhas:
 					for z in palavra_para_senhas:
 						for a in palavra_para_senhas:
@@ -129,13 +131,13 @@ MENU
 												quantidade_senhas_geradas += 1
 												armazenar = x+z+a+b+c+d+e+f
 												funcoes.adicionar(nomearq=arquivo,password=armazenar)
-				print("\033[32mStatus do arquivo \033[35m{}\033[m: \033[32mCRIADO\033[m".format(arquivo))
+				print("\033[32m[+] Status do arquivo\033[35m{}\033[m:\033[1;36mConcluído\033[m".format(arquivo))
 				print("\033[32mQuantidade de senhas no arquivo:\033[m {}".format(quantidade_senhas_geradas))
 				
 			elif userop == "F":
-				print("\033[31m[ATENÇÃO]: Por padrão, gera senhas com 6 caracteres!\033[m")
+				print("\033[1;33m[+] Tempo estimado: 6 horas {depende do dispositivo}\033[m")
 				time.sleep(1.4)
-				print("\033[31mGerando as suas senhas, este processo pode demorar...\033[m\n")
+				print("\033[1;31m[+] Gerando as suas senhas, este processo pode demorar...\033[m\n")
 				for x in palavra_aleatoria_para_senhas:
 					for z in palavra_aleatoria_para_senhas:
 						for a in palavra_aleatoria_para_senhas:
@@ -145,15 +147,17 @@ MENU
 										quantidade_senhas_geradas += 1
 										armazenar = x+z+a+b+c+d
 										funcoes.adicionar(nomearq=arquivo,password=armazenar)
-				print("\033[32mStatus do arquivo \033[35m{}\033[m: \033[32mCRIADO\033[m".format(arquivo))
-				print("\033[32mQuantidade de senhas no arquivo:\033[m {}".format(quantidade_senhas_geradas))
+				print("\033[32m[+] Status do arquivo\033[35m{}\033[m:\033[1;36mConcluído\033[m".format(arquivo))
+				print("\033[32m[+] Quantidade de senhas no arquivo:\033[m {}".format(quantidade_senhas_geradas))
 	else:
 		print("A extensão do arquivo não é permitida!")
 else:
 	print("""
 Para usar o programa use:
 
-\033[91mpython gerador.py senha.txt\033[m
+
+\033[91m[+] Use: python pyhash.py senha.txt\033[m
+
 
 no lugar de senha.txt você pode colocar o nome que
 desejar, desde que a extensão seja .txt
