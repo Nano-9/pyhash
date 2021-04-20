5b76c6947e99601dfa12ad173dcb3b2c8d93cd50
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2021-04-20 13:52:26 -0300

    Create arquivolog.py

---
+++ arquivolog.py
@@ -0,0 +1,31 @@
+# arquivo de log que mostra o ano o dia e a hora que o usuário está rodando o script principal
+
+import datetime
+import time
+
+def data_abertura_programa_principal():
+	"""
+	data : Armazena a hora certa que o arquivo foi aberto
+	pelo usuário
+	"""
+	data = str(datetime.datetime.now().time())
+
+
+def adicionar_dados_ao_arquivo(name,dados):
+	"""
+	Essa função tem como objetivo armazenar os dados
+	da função acima dentro de um arquivo de log
+	"""
+	try:
+		filelog = open(name, "at")
+	except Exception as e:
+		print("Erro Lucas")
+		print(e)
+	else:
+		try:
+			filelog.write("[+] Script iniciado as : {} \n".format(dados[0:8]))
+		except Exception as er:
+			print("Erro dois lucas")
+			print(er)
+		else:
+			pass
