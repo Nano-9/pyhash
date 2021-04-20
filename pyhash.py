e57f90aae2372d0aeb0e8a10845af9ef624affe5
Lucas Silva <69327287+lucas-Dk@users.noreply.github.com>
2021-04-20 14:12:22 -0300

    Update pyhash.py

--- pyhash.py
+++ pyhash.py
@@ -48,10 +48,10 @@ if len(logar) == 2:
 
 		dadoslog = arquivolog.data
 		print(dadoslog)
-		#arquivolog.adicionar_dados_ao_arquivo(name=logfile,dados=dadoslog)
+		arquivolog.adicionar_dados_ao_arquivo(name=logfile,dados=dadoslog)
 
 		print("\033[92m[+] Extensão de arquivo válida!\033[m")
-		time.sleep(5)
+		time.sleep(1.3)
 		os.system("clear")
 		print("""\033[94m
  _______  __   __  __   __  _______  _______  __   __ 
